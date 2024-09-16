7e205b119cd596547d1a7b330261f8c05f0f310b
Oláh András <106300817+oandris228@users.noreply.github.com>
2024-09-16 14:23:09 +0200

    idec

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { quotes } from './quotes';
 
 @Controller()
 export class AppController {
@@ -12,4 +13,37 @@ export class AppController {
       message: this.appService.getHello()
     };
   }
-}
+
+  @Get('/quotes')
+  @Render('quotes')
+  getQuotes() {
+    return quotes
+  }
+
+  @Get('/randomquote')
+  @Render('randomquote')
+  getRandomQuotes() {
+    return quotes
+  }
+
+  @Get('/topauthors')
+  @Render('topauthors')
+  getTopAuthors() {
+    let authors = new Map
+
+    for (let i = 0; i < quotes.quotes.length; i++) {
+      if (authors.has(quotes.quotes[i].author)){
+        authors.set(quotes.quotes[i].author, authors.get(quotes.quotes[i].author)+1);
+      }
+      else{
+        authors.set(quotes.quotes[i].author, 1);
+      }
+    }
+
+      authors = new Map([...authors.entries()].sort((a, b) => b[1] - a[1]));
+
+      return {
+        response: authors
+      };
+    };
+  }
